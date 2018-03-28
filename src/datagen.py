6f570d236e6e88ac1b76363c1f0b9aac335e692a
Todd DeLuca <nospam@todddeluca.com>
2018-03-27 23:54:52 -0400

    Finish homework

--- src/datagen.py
+++ src/datagen.py
@@ -3,6 +3,7 @@ import keras
 import numpy as np
 import pandas as pd
 import struct
+import random
 
 # See https://github.com/keras-team/keras/blob/master/examples/addition_rnn.py
 # for an example of not-padding by using np.zeros, so where there is no letter
@@ -44,11 +45,10 @@ import struct
 
 def text_to_sequences(text, seq_len, stride):
     '''
-    divide the text into sequences of length seq_len by 
-    striding it in strides of length stride and taking the subsequence 
-    of length seq_len at that position.
-
-    return: list of sequences
+    Divide text into training data by traversing text in strides of length stride.
+    At every step, collect a sequence of length seq_len and the character after the sequence.
+    
+    return: a list of sequences and a list of next characters, both the same length.
     '''
     sequences = []
     next_chars = []
@@ -56,39 +56,37 @@ def text_to_sequences(text, seq_len, stride):
     for i in range(0, text_len - (seq_len + 1), stride):
         j = i + seq_len
         if j < text_len:
-            seq = normalized_text[i:j]
-            next_char = normalized_text[j]
+            seq = text[i:j]
+            next_char = text[j]
             sequences.append(seq)
             next_chars.append(next_char)
     
-    sequences = [normalized_text[i:(i + seq_len)] 
-                 for i in range(0, len(normalized_text), stride)]
-    return sequences
+    return sequences, next_chars
 
 
-def preprocess_names():
+def preprocess_names(data_dir=None):
     '''
     return: a tuple containing: a text of female names, a text of male names.
     '''
-    names = load.load_names()
+    names = load.load_names(data_dir)
     male_text = ' '.join(names[names.sex == 'male'].name)
     female_text = ' '.join(names[names.sex == 'female'].name)
-    return (male_text, female_text)
+    return (female_text, male_text)
     
 
-def preprocess_jokes():
+def preprocess_jokes(data_dir=None):
     '''
     return: text containing jokes.
     '''
-    jokes = load.load_jokes()
+    jokes = load.load_jokes(data_dir)
     return ' '.join(jokes.body)
 
 
-def preprocess_pnp():
+def preprocess_pnp(data_dir=None):
     '''
     return: text of pride and prejudice.
     '''
-    pnp_text = load.load_pride_and_prejudice()
+    pnp_text = load.load_pride_and_prejudice(data_dir)
     # pnp has 31 header lines before the book title and 366 footer lines after the end of the book.
     # remove the project gutenberg header and footer
     return ' '.join(list(pnp_text.splitlines())[31:-366])
@@ -166,63 +164,77 @@ def normalize_text(text):
     it on text on whitespace and recombine the tokens using a 
     single space char.
     '''
-    return ' '.join(text.split()).lower()
+    return ' '.join(text.split()).lower().replace('\u201c', '"').replace('\u201d', '"')
 
 
-def get_text():
-     # get raw text
-    female_names_text, male_names_text = preprocess_names()
-    jokes_text = preprocess_names()
-    pnp_text = preprocess_pnp()
-    return normalize_text(male_names_text)
+def get_normalized_text(choice=None, data_dir=None):
+    if choice == 'female_names':     
+        female_names_text, male_names_text = preprocess_names(data_dir)
+        text = female_names_text
+    elif choice == 'pride':
+        text = preprocess_pnp(data_dir)
+        
+    elif choice == 'jokes':
+        text = preprocess_jokes(data_dir)
+    else:
+        female_names_text, male_names_text = preprocess_names(data_dir)
+        text = male_names_text
+        
+    return normalize_text(text)
 
 
-def get_tensors(text, seq_len, stride, vocab_size):
+def sequences_to_tensor(sequences, seq_len, char_to_int):
+    num_chars = max(char_to_int.values()) + 1
+    x = np.zeros((len(sequences), seq_len, num_chars))
+    for i, seq in enumerate(sequences):
+        for j, char in enumerate(seq):
+            try:
+                x[i, j, char_to_int[char]] = 1
+            except KeyError:
+                print(repr(char))
+                print(char.encode('ISO-8859-1'))
+                raise
+    
+    return x
+
+    
+def chars_to_tensor(chars, char_to_int):
+    num_chars = max(char_to_int.values()) + 1
+    y = np.zeros((len(chars), num_chars))
+    for i, char in enumerate(chars):
+        y[i, char_to_int[char]] = 1
+        
+    return y
+        
+    
+def text_to_tensors(text, seq_len, stride, vocab_size, num_seqs=None):
     '''
-    Return a tensor of shape (m, seq_len, vocab_size).  Each example is
+    Return a shuffled tensor of shape (num_seqs, seq_len, vocab_size).  Each example is
     created from the text source by sliding a window of size seq_len across
     the text, using a stride of stride. At each step, characters in the window 
     are converted to integers using ISO Latin 1, converting any incompatible characters
     to encodable ones as needed.  The integers are one hot encoded.
 
     seq_len: int, the "time" dimension in the recurrent sense.
-    The number of characters in the string.  Fixed for batch processing.
+      The number of characters in the string.  Fixed for batch processing.
     stride: int, the stride/step length of the sliding window.
-    '''    
-    
-    # convert chars to ints and back efficiently
-    int_to_char = get_int_to_char(vocab_size)
+    num_seqs: int or None.  if None, all sequences are returned (after being shuffled).  
+      Otherwise only num_seqs shuffled sequences are returned
+    '''        
     char_to_int = get_char_to_int(vocab_size)
-
     
-    # convert to overlapping sequences and next char
+    # convert text to overlapping sequences and next char
+    sequences, next_chars = text_to_sequences(text, seq_len, stride)
     
-    , to integers, to one-hot encoded vectors
-    raw_text = male_names_text # shape: (,)
-    text_x = text_to_sequences(raw_text, seq_len, stride) # shape: (m,)
-    padded_x = [pad_text(t, seq_len) for t in text_x] # shape: (m, n_t)
-    padded_y = [offset_by_one(t) for t in padded_x] # shape: (m, n_t)
-    int_x = np.array([text_to_ints(t, char_to_int) for t in padded_x]) # shape (m, n_t)
-    int_y = [text_to_ints(t, char_to_int) for t in padded_y] # shape (m, n_t)
-    x = keras.utils.to_categorical(int_x, num_classes=vocab_size) # shape (m, n_t, n_x)
-    y = keras.utils.to_categorical(int_y, num_classes=vocab_size) # shape (m, n_t, n_y)
+    # shuffle and down sample sequences
+    num_seqs = num_seqs if num_seqs is not None else len(sequences)
+    seqs_and_chars = random.sample(list(zip(sequences, next_chars)), num_seqs)
+    sequences, next_chars = zip(*seqs_and_chars)
     
+    # vectorize strings and characters
+    x = sequences_to_tensor(sequences, seq_len, char_to_int)
+    y = chars_to_tensor(next_chars, char_to_int)
     return x, y
-    
-     # convert string to list of ints, substituting chars that cannot be encoded
-#     def str_to_ints(s):
-#         return [i for i in s.replace('\u201c', '"').replace('\u201d', '"').encode('iso-8859-1')]
-    
-    # transform a list of lists of ints into an ndarray.
-#     return np.array(text.apply(str_to_ints).tolist())
-
-
-#     return text.apply(lambda x: x + (' ' * (length - len(x))))
-
-#     pad_text(male.text, n_t)
-
-#         return text.apply(lambda x: x[1:] + last_char)
-    
     
     
     
--- src/load.py
+++ src/load.py
@@ -3,24 +3,50 @@ Functions for loading datasets from the data dir
 '''
 
 import pandas as pd
-import config
+from pathlib import Path
 
-def load_names():
-    females = pd.read_csv(config.female_path, skiprows=6, header=None, names=['name'])
+
+def female_path(data_dir):
+    return data_dir / 'female_names.txt'
+
+
+def male_path(data_dir):
+    return data_dir / 'male_names.txt'
+
+
+def jokes_path(data_dir):
+    return data_dir / 'stupidstuff.json'
+
+
+def pnp_path(data_dir):
+    return data_dir / 'pride_and_prejudice.txt'
+
+
+def default_data_dir(data_dir):
+    return data_dir if data_dir else Path('~/data/2018/uvm_deep_learning_homework2/').expanduser()
+
+
+def load_names(data_dir=None):
+    data_dir = default_data_dir(data_dir)
+    females = pd.read_csv(female_path(data_dir), skiprows=6, header=None, names=['name'])
     females['sex'] = 'female'
-    males = pd.read_csv(config.male_path, skiprows=6, header=None, names=['name'])
+    males = pd.read_csv(male_path(data_dir), skiprows=6, header=None, names=['name'])
     males['sex'] = 'male'
 
     return pd.concat([females, males], ignore_index=True)
 
-def load_jokes():
+
+def load_jokes(data_dir=None):
     '''return a dataframe with columns: body, category, id, title.  body contains the text of the joke.'''
-    df = pd.read_json(config.jokes_path)
+    data_dir = default_data_dir(data_dir)
+    df = pd.read_json(jokes_path(data_dir))
     return df
 
-def load_pride_and_prejudice():
+
+def load_pride_and_prejudice(data_dir=None):
     '''return an unprocessed hunk of text with header lines prepended.'''
-    with open(config.pnp_path) as fh:
+    data_dir = default_data_dir(data_dir)
+    with open(pnp_path(data_dir)) as fh:
         text = fh.read()
         return text
     
